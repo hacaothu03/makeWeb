e8c3cfbd845a382e5c307f6dad42f7d88a33d044
Ha Cao Thu <ha.caothu.03@gmail.com>
2023-07-08 13:20:12 +0700

    Add new project student management

---
+++ app.js
@@ -0,0 +1,29 @@
+ // Hàm xử lý khi nhấn nút "Add Student"
+ function addStudent() {
+    // Gọi đến backend để thực hiện chức năng thêm sinh viên
+    // Xử lý các logic và hiển thị kết quả tương ứng
+}
+
+// Hàm xử lý khi nhấn nút "Edit Student"
+function editStudent() {
+    // Gọi đến backend để thực hiện chức năng chỉnh sửa sinh viên
+    // Xử lý các logic và hiển thị kết quả tương ứng
+}
+
+// Hàm xử lý khi nhấn nút "Delete Student"
+function deleteStudent() {
+    // Gọi đến backend để thực hiện chức năng xóa sinh viên
+    // Xử lý các logic và hiển thị kết quả tương ứng
+}
+
+// Hàm xử lý khi nhấn nút "View Student"
+function viewStudent() {
+    // Gọi đến backend để thực hiện chức năng xem chi tiết sinh viên
+    // Xử lý các logic và hiển thị kết quả tương ứng
+}
+
+// Hàm xử lý khi nhấn nút "View All Students"
+function viewAllStudents() {
+    // Gọi đến backend để thực hiện chức năng xem tất cả sinh viên
+    // Xử lý các logic và hiển thị kết quả tương ứng
+}
